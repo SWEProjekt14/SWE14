a0d112be02c51775893d118e9c1c4a796182dc89
aobuchoff <andrei.obuchoff@rwth-aachen.de>
2014-02-24 11:58:21 +0100

    Einfüegen und Suchen Datum ermoeglicht

--- Calendar/src/android/Calendar.java
+++ Calendar/src/android/Calendar.java
@@ -9,11 +9,15 @@ import org.json.JSONArray;
 
 import android.annotation.TargetApi;
 import android.content.ContentUris;
+import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Build;
+import android.provider.CalendarContract;
 import android.provider.CalendarContract.Events;
+import android.provider.ContactsContract.CommonDataKinds.Event;
+import android.widget.Toast;
 
 @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
 public class Calendar extends CordovaPlugin{
@@ -29,16 +33,16 @@ public class Calendar extends CordovaPlugin{
 		    if (ACTION_ADD_CALENDAR_ENTRY.equals(action)) {
 		    	Intent calIntent = new Intent(Intent.ACTION_EDIT)
 			        .setType("vnd.android.cursor.item/event")
-			    	// TODO: benoetigtes time-format bsp: 1392811200000
-			        // bekomme Format bsp: 1392811262697 ????
-			        .putExtra(Events.DTSTART, args.getLong(3))
-			        .putExtra(Events.DTEND, args.getLong(4))
+			        .putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, args.getLong(3))
+			        .putExtra(CalendarContract.EXTRA_EVENT_END_TIME, args.getLong(4))
 			        .putExtra(Events.TITLE, args.getString(0))
 			        .putExtra(Events.DESCRIPTION, args.getString(1))
 			        .putExtra(Events.EVENT_LOCATION, args.getString(2));
 		 
 		       this.cordova.getActivity().startActivity(calIntent);
 		       callback.success();
+		       
+		       
 		       return true;
 		    } else if(ACTION_DELETE_CALENDAR_ENTRY.equals(action)){
 		    	Uri deleteUri = ContentUris.withAppendedId(Events.CONTENT_URI, args.getLong(0));
@@ -46,6 +50,8 @@ public class Calendar extends CordovaPlugin{
 		    	callback.success();
 		    	return true;
 		    } else if(ACTION_SEARCH_CALENDAR_ENTRY.equals(action)){
+		    	String dBegin ="";
+		    	String dEnd="";
 		    	List<String> params = new ArrayList<String>();
 		    	String title = args.getString(0);
 		    	if(title == null || title.isEmpty()) title = "%";
@@ -54,21 +60,20 @@ public class Calendar extends CordovaPlugin{
 		    	String location = args.getString(2);
 		    	if(location == null || location.isEmpty()) location = "%";
 		    	String start = args.getString(3);
-		    	if(start == null || start.isEmpty()) start = "%";
-//		    	else params.add(start);
+		    	if(!(start == null || start.isEmpty()))
+		    		dBegin=(" AND " +Events.DTSTART + " = "+start);
 		    	String end = args.getString(4);
-		    	if(end == null || end.isEmpty()) end = "%";
-//		    	else params.add(end);
+		    	if(!(end == null || end.isEmpty()))
+		    		dEnd=(" AND " +Events.DTEND + " = "+end);
 		    	
 		    	params.add(title);
 		    	params.add(notes);
 		    	params.add(location);
 		    	String search = "("+Events.TITLE+" LIKE ? AND "+ 
 		    			Events.DESCRIPTION + " LIKE ? AND " +
-		    			Events.EVENT_LOCATION + " LIKE ?)";/*+
-		    			(start.equals("%")? "" : " AND "+Events.DTSTART + " = ?")+ 
-		    			(end.equals("%") ? "" : " AND "+Events.DTEND + " = ?") + ")";
-		    	*/
+		    			Events.EVENT_LOCATION + " LIKE ?"+ dBegin+dEnd+")";
+		    			
+		    	
 		    	Cursor cursor = cordova.getActivity().getContentResolver()
 		    			.query(Events.CONTENT_URI, new String[]{Events._ID}, search, params.toArray(new String[0]), null);
 		    	cursor.moveToFirst();
